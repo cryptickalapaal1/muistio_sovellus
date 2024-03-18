87e9a5d20a974cbb7201a27863d82ad249521a83
cryptickalapaal1 <veeti.taren@edu.riveria.fi>
2024-03-18 13:45:11 +0200

    sada

--- backend/server/app.js
+++ backend/server/app.js
@@ -16,6 +16,6 @@ app.use(express.json());
 const notesRoutes = require('./routes/notes');
 app.use('/api/notes', notesRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
